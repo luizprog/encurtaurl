3e9121a00813f4872db0f3f28048e54c05b70383
Luiz dos Santos <luiz@ssuark.com.br>
2022-01-31 11:19:04 -0300

    db location update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const sqlite3 = require('sqlite3').verbose();
 const swaggerUi = require('swagger-ui-express')
 swaggerDocument = require('./swagger.json');
 // open the database
-let db = new sqlite3.Database('C:/Users/luizteclastf5/github/encurtaurl/db/link.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('./db/link.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
   }
